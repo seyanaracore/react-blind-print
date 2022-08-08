3c6b4ef15f4e98b12df17055c55475e6242eba7a
Anton Tigunov <seyanaracore@gmail.com>
2022-08-08 18:10:28 +0300

    feat: control buttons component
    input state handlers

---
+++ src/components/ControlButtons/ControlButtons.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classes from './style.module.scss'
+
+function ControlButtons({ restartHandler, getNewTextHandler }) {
+  return (
+    <div className={classes.controlButtonsContainer}>
+      <button type="button" className="btn btn-primary" onClick={getNewTextHandler}>
+        Новый текст
+      </button>
+      <button type="button" className="btn btn-warning" onClick={restartHandler}>
+        Рестарт
+      </button>
+    </div>
+  )
+}
+
+ControlButtons.propTypes = {
+  restartHandler: PropTypes.func.isRequired,
+  getNewTextHandler: PropTypes.func.isRequired,
+}
+
+export default ControlButtons
