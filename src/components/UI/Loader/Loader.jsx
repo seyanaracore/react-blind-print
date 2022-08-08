ac11d2b9819e1347265ce1360ea85f437d4e21be
Anton Tigunov <seyanaracore@gmail.com>
2022-08-08 17:49:24 +0300

    feat: Loader UI
    UI components Loader

---
+++ src/components/UI/Loader/Loader.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import classes from './style.module.scss'
+
+const DIVS_COUNT = 12
+
+export default function Loader() {
+  return (
+    <div className={classes.ldsSpinner}>
+      {Array(DIVS_COUNT)
+        .fill(null)
+        .map((_, idx) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <div key={idx} />
+        ))}
+    </div>
+  )
+}
