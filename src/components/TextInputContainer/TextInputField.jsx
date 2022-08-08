a993b97b4560d970e2fcc7d9032e589918269e01
Anton Tigunov <seyanaracore@gmail.com>
2022-08-08 18:16:11 +0300

    feat: input filend component

---
+++ src/components/TextInputContainer/TextInputField.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef } from 'react'
+import PropTypes from 'prop-types'
+import useEventListener from '@/hooks/useEventListener'
+import classes from './style.module.scss'
+
+function TextInputField({ textForTyping, getClasses, inputHandler }) {
+  const textInput = useRef()
+
+  useEffect(() => {
+    textInput.current.focus()
+  }, [])
+
+  useEventListener('keydown', inputHandler, textInput.current)
+  return (
+    <div className={classes.inputFieldContainer} ref={textInput} role="textbox" tabIndex="-1">
+      {textForTyping.split('').map((char, idx) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <span key={idx} className={getClasses(idx)}>
+          {char}
+        </span>
+      ))}
+    </div>
+  )
+}
+
+TextInputField.propTypes = {
+  inputHandler: PropTypes.func.isRequired,
+  textForTyping: PropTypes.string.isRequired,
+  getClasses: PropTypes.func.isRequired,
+}
+
+export default TextInputField
